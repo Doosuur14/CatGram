dedb235f58467eb9a357baedae5d698c8a2b710a
Faki Doosuur <dorisfaki@gmail.com>
2023-07-28 18:04:44 +0300

    swift code on how to build an emoji lover app

---
+++ EmojiLover/EmojiLover/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  EmojiLover
+//
+//  Created by Faki Doosuur Doris on 28.07.2023.
+//
+
+import SwiftUI
+
+
+enum Emoji: String , CaseIterable{ // A list of cases you can define and pick from.
+    case  🥹,🥰,🥳,😱
+}
+
+struct ContentView: View {
+    @State var EmojiSelection: Emoji = .🥹
+    
+    
+    var body: some View {
+        NavigationView{
+            VStack{
+                Text(EmojiSelection.rawValue)
+                    .font(.system(size: 150))
+                
+                
+                Picker("Select Emoji", selection: $EmojiSelection){
+                    ForEach(Emoji.allCases, id:\.self){ emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .navigationTitle("My First App!")
+                .pickerStyle(.wheel)
+            }
+            .padding() //It helps to change the dimension of the picker
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider { //this is what shows the canvas on the right
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmojiLover/EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Faki Doosuur Doris on 28.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
