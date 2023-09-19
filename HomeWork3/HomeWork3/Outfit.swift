56eb9d66adfd65abacf023d7df7b8c7fc6805c98
Faki Doosuur <dorisfaki@gmail.com>
2023-09-20 01:24:06 +0300

    Setting AutoLayoutConstriants without storyboard

---
+++ HomeWork3/HomeWork3/Outfit.swift
@@ -0,0 +1,8 @@
+//
+//  Outfit.swift
+//  HomeWork3
+//
+//  Created by Faki Doosuur Doris on 24.09.2023.
+//
+
+import Foundation
---
+++ HomeWork3/HomeWork3/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  secondViewController.swift
+//  HomeWork3
+//
+//  Created by Faki Doosuur Doris on 23.09.2023.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
